ee566c6e63cb07c1a167cec978d5d5073ccbc6c9
Sebastian Beraud <beraudsebastian@gmail.com>
2020-11-11 16:03:33 +0100

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "La Buvette", category: "french", address: "Eberswalder Str. 120")
+Restaurant.create(name: "L'orologio", category: "italian", address: "Frankfurter Allee 80")
+Restaurant.create(name: "Pekin express", category: "chinese", address: "Karl-marx-str. 100")
+Restaurant.create(name: "Le Brussel", category: "belgian", address: "Danziger str. 309")
+Restaurant.create(name: "Japanese BBQ", category: "japanese", address: "Eberswalder Str. 80")
